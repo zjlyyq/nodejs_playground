c03db2e647639a6689ecad1db38bd361447449f0
zjl <zjl@win10Home>
2020-03-23 18:47:40 +0800

    例行提交

--- server_with_express/server/app.js
+++ server_with_express/server/app.js
@@ -1,20 +1,27 @@
+const path = require('path');
 const express = require("express");
 const app = express();
 const port = 3001;
-
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 app.use(
     "/react",
     (req, res, next) => {
-        // res.header("Access-Control-Allow-Origin", "*"); //自定义中间件，设置跨域需要的响应头。
+        res.header("Access-Control-Allow-Origin", "*"); //自定义中间件，设置跨域需要的响应头。
         next();
     },
-    express.static("D:/CodeIsMyLife/react_playground")
+    // express.static("D:/CodeIsMyLife/react_playground")
+    // express.static('../react_playground')
+    express.static('C:/Users/zjl/Documents/mycode/react_playground')
 );
 
-app.use("/root", express.static("public"));
+app.use("/root", 
+(req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*"); //自定义中间件，设置跨域需要的响应头。
+    next();
+},
+express.static("public"));
 
 // app.use(function(req, res, next) {
 //     res.status(404).send("Sorry can't find that!");
