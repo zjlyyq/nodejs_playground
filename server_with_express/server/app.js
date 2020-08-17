675e785e92a1a49b866c2af9c89dfce508c7ec95
tk_zjl <016760@zjtlcb.com>
2020-08-17 10:44:03 +0800

    增加地址转换文件

--- server_with_express/server/app.js
+++ server_with_express/server/app.js
@@ -15,6 +15,26 @@ app.use(
     // express.static('../react_playground')
     express.static('C:/Users/zjl/Documents/mycode/react_playground')
 );
+app.use(
+    "/tl",
+    (req, res, next) => {
+        res.header("Access-Control-Allow-Origin", "*"); //自定义中间件，设置跨域需要的响应头。
+        next();
+    },
+    // express.static("D:/CodeIsMyLife/react_playground")
+    // express.static('../react_playground')
+    express.static('/media/zjlyyq/AC0CDF290CDEED78/Users/tl_zjl/Documents/TL_CODE/dev-branch/多收多贷需求RCXQ2020-110001')
+);
+app.use(
+    "/es_basic",
+    (req, res, next) => {
+        res.header("Access-Control-Allow-Origin", "*"); //自定义中间件，设置跨域需要的响应头。
+        next();
+    },
+    // express.static("D:/CodeIsMyLife/react_playground")
+    // express.static('../react_playground')
+    express.static('C:/Users/tl_zjl/Documents/MyCode/es_basic')
+);
 
 app.use("/root", 
 (req, res, next) => {
